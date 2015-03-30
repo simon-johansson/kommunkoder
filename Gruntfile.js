d97da4ddbb4c2114a86de940d63e7b07a771fb5e
Simon Johansson <mail@simon-johansson.com>
2015-03-30 08:38:25 +0200

    Add dependencies to automize npm & bower releases

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,7 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     jshint: {
       options: {
         jshintrc: '.jshintrc',
@@ -23,7 +24,7 @@ module.exports = function (grunt) {
     },
     mochacli: {
       options: {
-        reporter: 'nyan',
+        reporter: 'spec',
         bail: true
       },
       all: ['test/*.js']
@@ -41,8 +42,66 @@ module.exports = function (grunt) {
         files: '<%= jshint.test.src %>',
         tasks: ['jshint:test', 'mochacli']
       }
+    },
+    browserify: {
+      dist: {
+        files: {
+          '<%= pkg.name %>.js': ['index.js']
+        },
+        options: {
+          browserifyOptions: {
+            standalone: '<%= pkg.name %>'
+          }
+        }
+      }
+    },
+    uglify: {
+      client: {
+        files: {
+          '<%= pkg.name %>.min.js': ['<%= pkg.name %>.js']
+        }
+      }
+    },
+    usebanner: {
+      options: {
+        position: 'top',
+        banner: [
+          '/*',
+          ' *   <%= pkg.name %> - v<%= pkg.version %>',
+          ' *   <%= pkg.description %>',
+          ' *   <%= pkg.homepage %>',
+          ' *   by <%= pkg.author.name %> <<%= pkg.author.email %>>',
+          ' *   <%= pkg.license %> License',
+          ' */\n'
+          ].join('\n'),
+        linebreak: true
+      },
+      files: {
+        src: ['<%= pkg.name %>.js', '<%= pkg.name %>.min.js']
+      }
+    },
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        updateConfigs: ['pkg'],
+        commitFiles: ['package.json', 'bower.json', '<%= pkg.name %>.js', '<%= pkg.name %>.min.js'],
+        pushTo: 'origin'
+      }
+    },
+    shell: {
+      publish: {
+        command: "npm publish"
+      }
     }
   });
 
   grunt.registerTask('default', ['jshint', 'mochacli']);
+
+  grunt.registerTask('client', ['browserify', 'usebanner']);
+
+  grunt.registerTask("release", "Release a new version, push it and publish it", function (target) {
+    var target = target ? target : "patch";
+    grunt.task.run("bump-only:" + target, "compile", "bump-commit", "shell:publish");
+  });
+
 };
